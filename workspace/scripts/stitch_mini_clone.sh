5d0b11f9841c8c3e78272578a6b0f01000d823bb
galk <gal.kaplon@mobileye.com>
2022-11-15 18:38:54 +0200

    debugging

--- workspace/scripts/stitch_mini_clone.sh
+++ workspace/scripts/stitch_mini_clone.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-python -m ipdb ../finetune_glue.py \
+python /opt/ml/code/workspace/finetune_glue.py \
        --do_train True --seed 0 --devid 0 --task mnli --src_model_name prajjwal1/bert-mini --model_dir ./saved_models/ \
        --model_name bert_mini-mini --do_stitch True --skip_layernorm False --stitch_dummy False --evaluation_strategy epoch \
        --save_strategy epoch --learning_rate 2e-5 --per_device_train_batch_size 64 --per_device_eval_batch_size 64 --num_train_epochs 5 \
