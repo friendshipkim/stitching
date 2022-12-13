a3e2f8f35597f9d8e5e6a84d14ef7811f7cb2d2b
GalKaplun <galkaplun@g.harvard.edu>
2022-12-13 18:49:17 +0000

    scripts

--- workspace/scripts/train_mini-mini-stitched.sh
+++ workspace/scripts/train_mini-mini-stitched.sh
@@ -1,13 +1,18 @@
-# training script for bert_mini-mini
+#! /usr/bin/env bash
 
-python finetune_glue.py \
+# training script for bert_mini
+
+OTHER_PARAMS=${@:1}
+export seed=$(($RANDOM%1000))
+
+/home/me.docker/.conda/envs/pl/bin/python ~/work/stitching/workspace/finetune_glue.py \
     --do_train True \
     --use_wandb True \
-    --seed 0 \
+    --seed $seed \
     --task mnli \
     --src_model_name prajjwal1/bert-mini \
     --model_dir ./saved_models/ \
-    --model_name bert_mini-mini \
+    --model_name bert_mini_seed_$seed \
     --do_stitch True \
     --skip_layernorm False \
     --stitch_dummy False \
@@ -16,6 +21,8 @@ python finetune_glue.py \
     --learning_rate 2e-5 \
     --per_device_train_batch_size 64 \
     --per_device_eval_batch_size 64 \
-    --num_train_epochs 5 \
+    --num_train_epochs 10 \
     --weight_decay 0.01 \
-    --load_best_model_at_end True
+    --load_best_model_at_end True \
+    --epsilon 0.001 \
+    $OTHER_PARAMS
